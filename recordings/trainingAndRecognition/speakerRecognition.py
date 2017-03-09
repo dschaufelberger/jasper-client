2ad3c12c1a5030e00439325903d46d10e2f94d61
RuWe <ruthweber95@web.de>
2017-03-09 23:00:05 +0100

    Changed speakerRecognition method

--- recordings/trainingAndRecognition/speakerRecognition.py
+++ recordings/trainingAndRecognition/speakerRecognition.py
@@ -1,5 +1,16 @@
 from piwho import recognition
-recog = recognition.SpeakerRecognizer('/home/pi/jasper/recordings/')
-name = recog.identify_speaker()
-print(name[0]) # Recognized speaker
-print(name[1]) # Second best speaker
+
+def greetings():
+    recog = recognition.SpeakerRecognizer('/home/pi/jasper/recordings/')
+    name = recog.identify_speaker()
+    return name[0]
+
+speaker = greetings()
+print speaker
+
+#recog = recognition.SpeakerRecognizer('/home/pi/jasper/recordings/trainingAndRecognition/')
+#name = recog.identify_speaker()
+#print(name[0]) # Recognized speaker
+#print(name[1]) # Second best speaker
+
+
